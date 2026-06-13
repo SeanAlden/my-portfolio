53a5c72727033c153997d8ed11db16ce26b260ed
Sean Alden <c14210052@john.petra.ac.id>
2026-06-13 20:44:15 +0700

    update next config ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,25 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'portfolio-images.cellar-c2.services.clever-cloud.com',
+        port: '',
+        pathname: '/**', // Mengizinkan semua path gambar di dalam bucket ini
+      },
+    ],
+  },
 };
 
 export default nextConfig;
